7b093e95d10162ff24bdcc28ec01343e05d74870
Chris Wen <chriswen96@gmail.com>
2023-12-26 22:27:07 -0800

    Updates

---
+++ precache-manifest.67963a333d6c92d15bf05db4b4d7bcb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77838a2d0282fbc28de731dadf6e4072",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8783c5858ec83937e1a",
+    "url": "/static/css/main.50f54215.chunk.css"
+  },
+  {
+    "revision": "d0555573ebc513c81373",
+    "url": "/static/js/2.babdf430.chunk.js"
+  },
+  {
+    "revision": "a8783c5858ec83937e1a",
+    "url": "/static/js/main.9a056ac5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
